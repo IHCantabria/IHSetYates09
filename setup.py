20782a1ac1d5a3350acd07f1969eb92bc9d5d021
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-05-29 10:01:33 +0200

    Correcting writer bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IHSetYates09',
-    version='1.4.6',
+    version='1.4.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -10,7 +10,6 @@ setup(
         'xarray',
         'numba',
         'pandas',
-        'math',
         'fast_optimization @ git+https://github.com/defreitasL/fast_optimization.git'
     ],
     author='Lucas de Freitas Pereira',
